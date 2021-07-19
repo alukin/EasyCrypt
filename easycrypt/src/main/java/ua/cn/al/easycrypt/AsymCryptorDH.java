a1ac552048ce8a322c98121838eb41d4cd9ff5a6
Oleksiy Lukin <alukin@gmail.com>
2021-07-19 08:04:28 +0300

    Copyright line

--- easycrypt/src/main/java/ua/cn/al/easycrypt/AsymCryptorDH.java
+++ easycrypt/src/main/java/ua/cn/al/easycrypt/AsymCryptorDH.java
@@ -1,4 +1,5 @@
 /*
+ * Copyright (C) 2018-2021 Oleksiy Lukin <alukin@gmail.com> and CONTRIBUTORS
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE
  * as published by the Free Software Foundation, version 3
